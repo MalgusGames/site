d345d24936d3757678abf8d0b75595fba4d947db
Kramer-Mekkaoui <umar.mekkaoui@gubagoo.com>
2025-06-12 19:52:50 -0500

    social media added

--- src/assets/components/Socials.jsx
+++ src/assets/components/Socials.jsx
@@ -1,8 +1,21 @@
 export default function Socials() {
   return (
-    <div>
-      <p>test</p>
-      <p>test</p>
+    <div className="socials">
+      <a
+        className="social-link"
+        href="https://www.instagram.com/kramermalgusgames/"
+      >
+        <div className="social-image instagram"></div>
+      </a>
+      <a
+        className="social-link"
+        href="https://www.tiktok.com/@malgus_games_dev"
+      >
+        <div className="social-image tik-tok"></div>
+      </a>
+      <a className="social-link" href="https://www.youtube.com/@malgusgames">
+        <div className="social-image youtube"></div>
+      </a>
     </div>
   );
 }
