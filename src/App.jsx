0c942c3584fbe688a80fa9a1f4420700fbada4d3
Kramer-Mekkaoui <umar.mekkaoui@gubagoo.com>
2025-06-12 19:12:30 -0500

    social test

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
-import malguslogo from './assets/Malgus Logo.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import malguslogo from "./assets/Malgus Logo.png";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Socials from "./assets/components/Socials";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <img className='logo' src={malguslogo} alt="" />
+        <img className="logo" src={malguslogo} alt="" />
       </div>
       <h1>Under Construction</h1>
       <p className="read-the-docs">
-        Our site is undergoing a much needed facelift. It may be many a fortnight before we're back up, but feel free to check us out on our socials in the meanwhile!
+        Our site is undergoing a much needed facelift. It may be many a
+        fortnight before we're back up, but feel free to check us out on our
+        socials in the meanwhile!
       </p>
+      <Socials />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/components/Socials.jsx
@@ -0,0 +1,8 @@
+export default function Socials() {
+  return (
+    <div>
+      <p>test</p>
+      <p>test</p>
+    </div>
+  );
+}
