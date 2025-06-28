6979c36d32e59b18f2736aedb1b7400ed6b8aa1a
Kramer-Mekkaoui <umar.mekkaoui@gubagoo.com>
2025-06-28 14:51:33 -0500

    import logo for nav

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,15 +1,12 @@
 import Socials from "../components/Socials";
+import logo from "/src/assets/Nav-Home-Icon.png";
 
 export default function Nav() {
   return (
     <div className="navigation">
       <div className="nav-home">
         <a href="https://www.malgusgames.com/" className="homepage-name">
-          <img
-            className="nav-home-icon"
-            src="/src/assets/Nav-Home-Icon.png"
-            alt=""
-          />
+          <img className="nav-home-icon" src={logo} alt="" />
           Malgus Games
         </a>
       </div>
